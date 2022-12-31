7fb0bbea564f881dba945f4ac2e32ae181c5a7df
ChickenCurry3034 <106786326+ChickenCurry3034@users.noreply.github.com>
2022-12-31 12:18:34 -0500

    Update Reapr_TFOD_Webcam.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reapr_TFOD_Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reapr_TFOD_Webcam.java
@@ -62,7 +62,7 @@ public class Reapr_TFOD_Webcam extends LinearOpMode {
      * has been downloaded to the Robot Controller's SD FLASH memory, it must to be loaded using loadModelFromFile()
      * Here we assume it's an Asset.    Also see method initTfod() below .
      */
-    private static final String TFOD_MODEL_ASSET = "Reapr_model_unquant.tflite";
+    private static final String TFOD_MODEL_ASSET = "model_unquant.tflite";
     // private static final String TFOD_MODEL_FILE  = "/sdcard/FIRST/tflitemodels/CustomTeamModel.tflite";
 
 
